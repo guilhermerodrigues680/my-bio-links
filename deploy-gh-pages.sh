7b81652608758f940042b78f27c02aa5d1d44797
guilhermerodrigues680 <guilhermerodrigues680@gmail.com>
2022-07-10 10:51:59 -0300

    deploy GitHub Pages

---
+++ deploy-gh-pages.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env sh
+
+# https://vitejs.dev/guide/static-deploy.html#github-pages
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+echo 'bio.guiodev.com' > CNAME
+
+git init
+git checkout -b main
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+git push -f git@github.com:guilhermerodrigues680/my-bio-links.git main:gh-pages
+
+rm -rf .git
+
+cd -
