d7104d2fd8ce79eaa2232f6ee350e145dedd64fc
guilhermerodrigues680 <guilhermerodrigues680@gmail.com>
2022-07-10 08:28:31 -0300

    melhoria css button3d

--- src/components/Button3d/Button3d.jsx
+++ src/components/Button3d/Button3d.jsx
@@ -3,7 +3,11 @@ import styles from "./Button3d.module.css";
 console.debug("styles", styles);
 
 function Button3d({ color = "red" }) {
-  return <button className={styles["button-3d"]}>Button3d</button>;
+  return (
+    <button className={styles["button-3d"]} type="button">
+      Button3d
+    </button>
+  );
 }
 
 export default Button3d;
