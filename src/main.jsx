f55d106f7a70d4c707464885e7ceb6089929414c
guilhermerodrigues680 <guilhermerodrigues680@gmail.com>
2022-07-10 09:19:37 -0300

    ajusta cores e fontes

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+
+// Self-hosting font Raleway
+import "@fontsource/raleway/400.css";
+import "@fontsource/raleway/700.css";
 import "./styles/global.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
